fc8abc52b826a2636a7e387b25796649c1e1a352
Gustavo Kreuch <gustavokreuch40@gmail.com>
2026-08-31 21:24:03 -0300

    Initial commit

---
+++ src/test/java/com/kreuch/calculadoura/CalculadouraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kreuch.calculadoura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadouraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
